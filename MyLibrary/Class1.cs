c60b94323b96c360df9476a60ed150205a8cc678
Matthias Wenz <matwen@microsoft.com>
2017-10-04 07:45:59 -0700

    Make use of json library

--- MyLibrary/Class1.cs
+++ MyLibrary/Class1.cs
@@ -1,10 +1,15 @@
 ﻿using System;
+using Newtonsoft.Json;
 
 namespace MyLibrary
 {
     public class Class1
     {
         public Class1() {
+            string json = "{}";
+
+            Object o = JsonConvert.DeserializeObject<Object>(json);
+            
             throw new AccessViolationException();
         }
     }
